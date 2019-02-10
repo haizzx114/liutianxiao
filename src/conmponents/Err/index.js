bb5088c366dd2780f9ce8762adff1c3a23fa6b76
haizzx114 <47495972+haizzx114@users.noreply.github.com>
2019-02-10 19:05:47 +0800

    Add files via upload

--- src/conmponents/Err/index.js
+++ src/conmponents/Err/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css'
-
+import logo from "../../img/err.jpg"
+import {Link} from "react-router-dom"
+import "./index.css"
 
 class Err extends Component {
   render() {
     return (
-      <div >
-          404
+      <div className = "err">
+          <Link to={`/home`}>
+              <img src={logo} alt="" className = "logo"/>
+            </Link>
       </div>
     );
   }
